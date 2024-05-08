8f0de1f6640251f50c451ba2cac8e01f394cb7ec
Luis Caraballo <lecmbogota@gmail.com>
2024-05-08 16:39:47 -0500

    actualizacion de mensaje de finalizacion

--- flows/finaliza.flow.js
+++ flows/finaliza.flow.js
@@ -3,7 +3,7 @@ const { addKeyword } = require('@bot-whatsapp/bot')
  *  Flujo de finalizacion 
  */
 module.exports = addKeyword('#_/FINALIZA/_#').addAction(async (_, { endFlow }) => {
-    return endFlow('🎉 GRACIAS POR SU VISITA 🎉')
+    return endFlow('🎉 ¡Hola! Parece que nuestra conversación ha llegado a su fin. Agradezco mucho tu interés y tiempo dedicado. Antes de despedirnos, te animo a que explores más ofertas y productos increíbles en nuestra página web: https://clysamoda.com/ ¡No te pierdas las oportunidades que te esperan! Si necesitas algo más, ¡aquí estaré! ¡Hasta pronto y que tengas un gran día! 🌟')
     }
 )
 
